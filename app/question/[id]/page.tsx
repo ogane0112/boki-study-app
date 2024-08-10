37852d738e24cb81fd77e016434b889fb2225116
ogane0112 <kai513272@gmail.com>
2024-08-10 22:35:20 +0900

    ビルドやっと通った

--- app/question/[id]/page.tsx
+++ app/question/[id]/page.tsx
@@ -1,27 +1,34 @@
-// app/question/[id]/page.tsx
-//ログインなしで解くことのできる
 import { createClient } from '@/utils/supabase/server';
 import type { PostgrestResponse } from '@supabase/supabase-js'; 
 import Question from "@/components/Question"
-export default async function QuestionPage({ params }: { params: { id: string } }) {
-  const supabase = createClient();
-  const { id } = params;
 
-  const { data: userAnswers, error } = await supabase
-    .from('questions')
-    .select('*')
-    .eq('question_id', id);
-
-  if (error) {
-    console.error(error);
-  }else{
-    console.log("よくここ見てくれたね( ´∀｀ )")
-    console.log(userAnswers)
-  }
-
-  return (
-    <>
-        <Question props = {userAnswers} />
-    </>
-  );
+interface QuestionProps {
+    props: { 
+        question_id: number; 
+        question_text: string; 
+        correct_debit_entries: { [key: string]: number }; 
+        correct_credit_entries: { [key: string]: number }; 
+        explanation?: string; 
+    }[] 
 }
+
+export default async function QuestionPage({ params }: { params: { id: string } }) {
+  const supabase = createClient();
+  const { id } = params;
+
+  const { data: userAnswers, error } = await supabase
+    .from('questions')
+    .select('*')
+    .eq('question_id', id);
+
+  if (error || userAnswers.length === 0) {
+    return <div>問題が見つかりませんでした</div>;
+  }
+
+  return (
+    <>
+        <Question props={userAnswers} />
+    </>
+  );
+}
+
