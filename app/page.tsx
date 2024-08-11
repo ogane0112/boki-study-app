df92d862d5cff9acc8c96f88e337245403bb3abc
ogane0112 <kai513272@gmail.com>
2024-08-11 17:12:43 +0900

    いらんものを消しました

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import Header from "@/components/Header";
@@ -21,7 +20,6 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
